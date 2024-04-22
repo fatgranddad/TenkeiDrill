0eae03057e3f8acfed3a877b9d8cdbe8455ddb45
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-04-22 23:08:08 +0900

    model間のアソシエーション

--- app/models/hand_type.rb
+++ app/models/hand_type.rb
@@ -1,2 +1,3 @@
 class HandType < ApplicationRecord
+  has_many :hand_results, dependent: :destroy
 end
--- app/models/tile_set.rb
+++ app/models/tile_set.rb
@@ -1,3 +1,6 @@
 class TileSet < ApplicationRecord
   belongs_to :user
+  has_many :tile_set_items, dependent: :destroy
+  has_many :tiles, through: :tile_set_items
+  has_many :hand_results, dependent: :destroy
 end
